a1f81d606fd4e214da802c3ec809e4b0c154eff9
Макеева Татьяна <tyh94@mail.ru>
2026-01-11 14:18:54 +0700

    Add keys as enum

--- Sources/Storage/KeyValueStorage/KeyValueStorage.swift
+++ Sources/Storage/KeyValueStorage/KeyValueStorage.swift
@@ -12,3 +12,17 @@ public protocol KeyValueStorage: Sendable {
     func object<T: Codable>(forKey key: String) throws -> T?
     func removeObject(forKey key: String) throws
 }
+
+extension KeyValueStorage {
+    public func set<T: Codable>(_ value: T, forKey key: some RawRepresentable<String>) throws {
+        try set(value, forKey: key.rawValue)
+    }
+    
+    public func object<T: Codable>(forKey key: some RawRepresentable<String>) throws -> T? {
+        try object(forKey: key.rawValue)
+    }
+    
+    public func removeObject(forKey key: some RawRepresentable<String>) throws {
+        try removeObject(forKey: key.rawValue)
+    }
+}
