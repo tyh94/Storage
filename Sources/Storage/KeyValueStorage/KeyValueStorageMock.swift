ad059b3faf41dccd3f16c172885c06b250180fa4
Макеева Татьяна <tyh94@mail.ru>
2026-06-24 10:26:17 +0700

    Add params to keyvalue mock

--- Sources/Storage/KeyValueStorage/KeyValueStorageMock.swift
+++ Sources/Storage/KeyValueStorage/KeyValueStorageMock.swift
@@ -8,13 +8,19 @@
 import Foundation
 
 public struct KeyValueStorageMock: KeyValueStorage {
-    public init() {}
-    public func set<T: Codable>(_ value: T, forKey key: String) throws {
+    let objects: [String: Data]
+    
+    public init(objects: [String: Data] = [:]) {
+        self.objects = objects
+    }
+    
+    public func set<T>(_ value: T, forKey key: String) throws where T : Decodable, T : Encodable {
         
     }
     
-    public func object<T: Codable>(forKey key: String) throws -> T? {
-        nil
+    public func object<T>(forKey key: String) throws -> T? where T : Decodable, T : Encodable {
+        guard let data = objects[key] else { return nil }
+        return try JSONDecoder().decode(T.self, from: data)
     }
     
     public func removeObject(forKey key: String) throws {
